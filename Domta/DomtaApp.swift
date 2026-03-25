90630fbe04a0d9bc71204598c6a5df1b5efef787
Ratchapol Vanavichit <zeroasd1114@outlook.com>
2026-03-25 13:20:23 +0700

    Initial Commit

---
+++ Domta/DomtaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DomtaApp.swift
+//  Domta
+//
+//  Created by Ratchapol Vanavichit on 25/3/26.
+//
+
+import SwiftUI
+
+@main
+struct DomtaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
